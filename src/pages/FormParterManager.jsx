6631ee24abc24bed4f9078baeff9150eedae8a19
antqd <160028748+antqd@users.noreply.github.com>
2025-09-09 17:57:17 +0200

    Nuove funzioni invio dojo diventa partner manager FUNZIONANTE

--- src/pages/FormParterManager.jsx
+++ src/pages/FormParterManager.jsx
@@ -1,10 +1,8 @@
-// src/pages/OnboardingAzienda.jsx
 import React, { useRef, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const API_URL = "https://emailsender-68kp.onrender.com/api/diventa-partner-manager";
 
- // <<-- cambia qui
 
 export default function OnboardingAzienda() {
   const [form, setForm] = useState({
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vite.dev/config/ and https://tailwindcss.com/docs/guides/vite
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
